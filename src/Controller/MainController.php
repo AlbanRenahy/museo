c44940ab15361fb7e76fdaeaafda36019d52196a
amtbzh <amtbzh@gmail.com>
2020-02-27 14:53:36 +0100

    Makes all migration

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\MonumentRepository;
+use App\Repository\RegionRepository;
+use App\Repository\StatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +13,12 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(MonumentRepository $monumentRepository, RegionRepository $regionRepository, StatusRepository $statusRepository)
     {
-        return $this->render('main/index.html.twig');
+        return $this->render('main/index.html.twig', [
+            'monuments' => $monumentRepository->findAll(),
+            'regions' => $regionRepository->findAll(),
+            'status' => $statusRepository->findAll(),
+        ]);
     }
 }
