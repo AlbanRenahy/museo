4481fe2efc44d720e58ded270afe1ce7cd0d3eb1
amtbzh <amtbzh@gmail.com>
2020-02-28 16:08:08 +0100

    continue blog CRUD

--- src/Controller/AdminBlogController.php
+++ src/Controller/AdminBlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,12 +11,31 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AdminBlogController extends AbstractController
 {
+
     /**
      * @Route("/", name="admin.blog.index")
      */
-    public function index()
+    public function index(BlogRepository $blogRepository)
     {
-        return $this->render('dashboard/blog/index.html.twig');
+        return $this->render('dashboard/blog/index.html.twig', [
+            'articles' => $blogRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin.blog.edit", methods="GET|POST")
+     */
+    public function edit()
+    {
+        // TODO: form for edit
+    }
+
+    /**
+     * @Route("/{id}/delete", name="admin.blog.delete", methods="DELETE")
+     */
+    public function delete()
+    {
+        // TODO: delete single post
     }
 
 }
