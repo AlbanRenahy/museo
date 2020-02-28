e31e2b1ad960f775f376ff10433aabdd5d26da04
amtbzh <amtbzh@gmail.com>
2020-02-28 14:11:26 +0100

    Structure OK

--- src/Controller/MonumentController.php
+++ src/Controller/MonumentController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/admin/monument")
+ */
 class MonumentController extends AbstractController
 {
 
+    /**
+     * @Route("/", name="admin.monument.index")
+     */
     public function index()
     {
-        return $this->render('monument/index.html.twig');
+        return $this->render('dashboard/monument/index.html.twig');
     }
 }
