3d18a73da52ef8b305d56e8c76e5e6caef50d12a
amtbzh <amtbzh@gmail.com>
2020-02-28 10:10:02 +0100

    added Form Login Logout Registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -33,6 +33,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        return null;
+        return $this->redirectToRoute('app_login');
     }
 }
