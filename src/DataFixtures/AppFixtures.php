8c1b53b71a7bf69aca845515089205615b8f0068
julienh44 <julien-hillion@hotmail.fr>
2020-02-28 15:13:42 +0100

    ajout de la table period + des data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Period;
+use App\Entity\Monument;
+use App\Entity\Region;
+use App\Entity\Status;
+use App\Entity\Target;
+use App\Entity\Thematic;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +15,46 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $monument = new Monument();
+            $monument->setName('monument '.$i);
+            $monument->setCity('city '.$i);
+            $monument->setAdress('adress '.$i);
+            $monument->setDescription('description' .$i);
+            $monument->setThematic('thematiques' .$i);
+            $manager->persist($monument);
+        }
 
-        $manager->flush();
+        for ($i = 0; $i < 20; $i++) {
+            $period = new Period();
+            $period->setName('period '.$i);
+            $manager->persist($period);
+        }
+
+        for ($i = 0; $i < 20; $i++) {
+            $region = new Region();
+            $region->setName('region '.$i);
+            $manager->persist($region);
+        }
+        
+        for ($i = 0; $i < 20; $i++) {
+            $status = new Status();
+            $status->setStatus($i);
+            $manager->persist($status);
+        }
+
+        for ($i = 0; $i < 20; $i++) {
+            $target = new Target();
+            $target->setName('target '.$i);
+            $manager->persist($target);
+        }
+
+        for ($i = 0; $i < 20; $i++) {
+            $thematic = new Thematic();
+            $thematic->setName('thematic '.$i);
+            $manager->persist($thematic);
+        }
+
+            $manager->flush();
     }
 }
