360273d7a28fb2b6a8f983bd20ec7e640d8c2cc1
amtbzh <amtbzh@gmail.com>
2020-02-28 14:21:32 +0100

    change some fixtures

--- src/Controller/AdminMonumentController.php
+++ src/Controller/AdminMonumentController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/admin/monument")
  */
-class MonumentController extends AbstractController
+class AdminMonumentController extends AbstractController
 {
 
     /**
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,16 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/blog")
+ */
 class BlogController extends AbstractController
 {
+
     /**
-     * @Route("/blog", name="blog.index")
+     * @var BlogRepository
+     */
+    private $repository;
+
+    public function __construct(BlogRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/", name="blog.index")
      */
     public function index()
     {
-        return $this->render('blog/index.html.twig');
+        $posts = $this->repository->findAll();
+        return $this->render('blog/index.html.twig', compact('posts'));
+    }
+
+    public function show()
+    {
+        return $this->render('blog/show.html.twig');
     }
 }
