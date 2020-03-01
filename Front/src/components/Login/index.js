439287c91d69c36c024206b8c17adb18e7b5917b
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-03-01 10:21:44 +0100

    Refactoring: creation of Form & Input components

--- Front/src/components/Login/index.js
+++ Front/src/components/Login/index.js
@@ -6,10 +6,12 @@ import { Link } from 'react-router-dom';
 
 // images
 // import MuseoLogoBW from '../../styles/images/museo_logo.png';
-import MuseoLogoBW from 'src/styles/images/logo_museo_couleur.png';
+import MuseoLogo from 'src/styles/images/logo_museo_couleur.png';
 import Background from 'src/styles/images/BG-Login-Signin.png';
 
 // Composants
+import Form from '../Form';
+import Input from '../Input';
 
 // Styles et assets
 import './login.scss';
@@ -17,53 +19,41 @@ import './login.scss';
 /*
  * Code
  */
-const Login = () => {
-  // Je récupère le focus d'un input
-  const handleFocus = (e) => {
-    // J'ajoute la classe focus à l'élément focus, ce qui passe sa bordure en violet.
-    e.target.previousSibling.classList.add('open', 'focus');
-    e.target.classList.add('focus');
-  };
-
-  // Je récupère le blur d'un input
-  const handleBlur = (e) => {
-    // Je remove la classe focus, ce qui redonnera à l'élément la bordure de base
-    e.target.previousSibling.classList.remove('focus');
-    e.target.classList.remove('focus');
-  };
-
-  return (
-    <div
-      className="login"
-      style={{
-        backgroundImage: `url(${Background})`,
-      }}
-    >
-      <div className="login">
-        <div className="login-container">
-          <img src={MuseoLogoBW} alt="Museo" />
-          <p className="login-container_subtitle">Application cartographique pour vos monuments et musées favoris </p>
-          <form action="">
-            <div>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" placeholder="Email" onClick={handleFocus} onBlur={handleBlur} />
-            </div>
-            <div>
-              <label htmlFor="password">Mot de passe</label>
-              <input type="password" id="password" name="password" placeholder="Mot de passe" onClick={handleFocus} onBlur={handleBlur} />
-            </div>
-            <p className="lost-password">
-              <a href="#">J'ai perdu mon mot de passe</a>
-            </p>
-            <button type="submit">Me connecter</button>
-            <Link to="/signin">Je souhaite m'inscrire</Link>
-            <Link to="/map">Entrer en simple visite</Link>
-          </form>
-        </div>
+const Login = () => (
+  <div
+    className="login"
+    style={{
+      backgroundImage: `url(${Background})`,
+    }}
+  >
+    <div className="login">
+      <div className="login-container">
+        <img src={MuseoLogo} alt="Museo" />
+        <p className="login-container_subtitle">Application cartographique pour vos monuments et musées favoris </p>
+        <Form>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+          />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Mot de passe"
+          />
+          <p className="lost-password">
+            <a href="#">J'ai perdu mon mot de passe</a>
+          </p>
+          <button type="submit">Me connecter</button>
+          <Link to="/signin">Je souhaite m'inscrire</Link>
+          <Link to="/map">Entrer en simple visite</Link>
+        </Form>
       </div>
     </div>
-  );
-};
+  </div>
+ );
 
 /**
  * Export
--- Front/src/components/Signin/index.js
+++ Front/src/components/Signin/index.js
@@ -9,84 +9,79 @@ import { Link } from 'react-router-dom';
  * Local import
  */
 // Composants
-import MuseoLogoBW from 'src/styles/images/logo_museo_couleur.png';
+import MuseoLogo from 'src/styles/images/logo_museo_couleur.png';
 import Background from 'src/styles/images/BG-Login-Signin.png';
+import Form from '../Form';
+import Input from '../Input';
+
 // Styles et assets
 import './signin.scss';
 
 /**
  * Code
  */
-const Signin = () => {
-
-  // Je récupère le focus d'un input
-  const handleFocus = (e) => {
-    // J'ajoute la classe focus à l'élément focus, ce qui passe sa bordure en violet.
-    console.log(e.target.previousSibling);
-    e.target.previousSibling.classList.add('open', 'focus');
-    e.target.classList.add('focus');
-  };
-
-  // Je récupère le blur d'un input
-  const handleBlur = (e) => {
-    // Je remove la classe focus, ce qui redonnera à l'élément la bordure de base
-    e.target.previousSibling.classList.remove('focus');
-    e.target.classList.remove('focus');
-  };
-
-  return (
-    <div
-      className="login"
-      style={{
-        backgroundImage: `url(${Background})`,
-      }}
-    >
+const Signin = () => (
+  <div
+    className="login"
+    style={{
+      backgroundImage: `url(${Background})`,
+    }}
+  >
     <div className="signin">
       <div className="signin-container">
 
 
-        <h1><img src={MuseoLogoBW} alt="Museo" /></h1>
+        <h1><img src={MuseoLogo} alt="Museo" /></h1>
         <p className="login-container_subtitle">Application cartographique pour vos monuments et musées favoris </p>
 
 
-        <form action="">
+        <Form>
           <div className="signin-container_names">
-            <div>
-              <label htmlFor="last-name">Nom</label>
-              <input type="text" id="last-name" name="last-name" placeholder="Nom" onClick={handleFocus} onBlur={handleBlur} />
-            </div>
+            <Input
+              type="text"
+              id="last-name"
+              name="last-name"
+              placeholder="Nom"
+            />
 
-            <div>
-              <label htmlFor="first-name">Prénom</label>
-              <input type="text" id="first-name" name="first-name" placeholder="Prénom" onClick={handleFocus} onBlur={handleBlur} />
-            </div>
+            <Input
+              type="text"
+              id="first-name"
+              name="first-name"
+              placeholder="Prénom"
+            />
           </div>
 
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Email" onClick={handleFocus} onBlur={handleBlur} />
-          </div>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+          />
 
-          <div>
-            <label htmlFor="password">Mot de passe</label>
-            <input type="password" id="password" name="password" placeholder="Mot de passe" onClick={handleFocus} onBlur={handleBlur} />
-          </div>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Mot de passe"
+          />
 
-          <div>
-            <label htmlFor="password-confirm">Confirmer votre mot de passe</label>
-            <input type="password" id="password-confirm" name="password-confirm" placeholder="Confirmer votre mot de passe" onClick={handleFocus} onBlur={handleBlur} />
-          </div>
+          <Input
+            type="password"
+            id="password-confirm"
+            name="password-confirm"
+            placeholder="Confirmer votre mot de passe"
+          />
 
           <button type="submit">M'inscrire</button>
 
           <p className="map-link"><Link to="/login">J'ai déjà mes identifiants</Link></p>
-        </form>
-
+        </Form>
       </div>
     </div>
-</div>
-  );
-};
+  </div>
+
+);
 
 /**
  * Export
