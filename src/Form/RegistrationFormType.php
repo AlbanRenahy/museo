5b4fb01ab876de4c122ec3ea8effd7fe4d667acf
amtbzh <amtbzh@gmail.com>
2020-03-03 10:01:08 +0100

    fix: Registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,32 +19,31 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => "Vous devez accepter les termes d'utilisation",
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Entrer un mot de passe',
+        ->add('email', EmailType::class)
+        ->add('firstname', TextType::class)
+        ->add('lastname', TextType::class)
+        ->add('plainPassword', PasswordType::class, [
+            'mapped' => false,
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 3,
                         'minMessage' => 'Le mot de passe de faire au minimum {{ limit }} caracteres',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+                        ]),
+                    ],
+        ])
+        ->add('agreeTerms', CheckboxType::class, [
+            'mapped' => false,
+            'constraints' => [
+                new IsTrue([
+                    'message' => "Vous devez accepter les termes d'utilisation",
+                ]),
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
