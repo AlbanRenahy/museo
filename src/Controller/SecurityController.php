0d96c8a170a210677c80f7a37b31da5ce1eedd6c
amtbzh <amtbzh@gmail.com>
2020-03-05 10:43:18 +0100

    add: Login Path for Front React

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -24,6 +25,20 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/api/login", name="login_front", methods="POST")
+     */
+    public function login_front(Request $req)
+    {
+        $user = $this->getUser();
+
+        return $this->json([
+            'username' => $user->getUsername(),
+            'password' => $user->getPassword(),
+            'roles' => $user->getRoles(),
+        ]);
     }
 
     /**
