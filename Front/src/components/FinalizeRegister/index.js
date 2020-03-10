ae7156031dd8b32b986c40769bf6ebaa2834abc3
CecileGit <ceciled2013@gmail.com>
2020-03-10 16:03:34 +0100

    Improved Done

--- Front/src/components/FinalizeRegister/index.js
+++ Front/src/components/FinalizeRegister/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Message } from 'semantic-ui-react';
+import { Message, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import './finalizeRegister.scss';
 
 
 const FinalizeRegister = () => (
-  <Message floating className='finalMessage' color='purple' size='big'>
-    <Message.Header>Il ne vous reste qu'une étape :</Message.Header>
-    <p>Cliquez sur le lien que nous vous avons envoyé par Email</p>
-    <Link to="/">Retour à la page d'accueil</Link>
-  </Message>
+  <Grid className="finalizeRegister" container columns={1}>
+    <Grid.Column textAlign="center">
+      <Message floating className="finalMessage" color="purple" size="big">
+        <Message.Header>Il ne vous reste qu'une étape :</Message.Header>
+        <p>Cliquez sur le lien que nous vous avons envoyé par Email</p>
+        <Link to="/">Retour à la page d'accueil</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
 );
 
 export default FinalizeRegister;
-
-
-
