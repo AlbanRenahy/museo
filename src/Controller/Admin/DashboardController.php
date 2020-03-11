0e86c35f206864ccb7543281e5006602e0a56b4b
amtbzh <amtbzh@gmail.com>
2020-03-12 00:22:03 +0100

    finish: All count in Dashboard Homepage

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CategoryRepository;
 use App\Repository\MonumentRepository;
 use App\Repository\PeriodRepository;
 use App\Repository\RegionRepository;
 use App\Repository\TargetRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 /**
@@ -14,17 +16,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
 
+    public function __construct(MonumentRepository $monumentRepository, RegionRepository $regionRepository, PeriodRepository $periodRepository, TargetRepository $targetRepository, UserRepository $userRepository, CategoryRepository $categoryRepository)
+    {
+        $this->countMonuments = $monumentRepository->countByEntry();
+        $this->countRegions = $regionRepository->countByEntry();
+        $this->countPeriods = $periodRepository->countByEntry();
+        $this->countTargets = $targetRepository->countByEntry();
+        $this->countUsers = $userRepository->countByEntry();
+        $this->countCategories = $categoryRepository->countByEntry();
+    }
 
     /**
      * @Route("/", name="index")
      */
-    public function index(MonumentRepository $monumentRepository, RegionRepository $regionRepository, PeriodRepository $periodRepository, TargetRepository $targetRepository)
+    public function index()
     {
         return $this->render('dashboard/index.html.twig', [
-            'countMonuments' => $monumentRepository->countByEntry(),
-            'countRegions' => $regionRepository->countByEntry(),
-            'countPeriods' => $periodRepository->countByEntry(),
-            'countTargets' => $targetRepository->countByEntry(),
+            'countMonuments' => $this->countMonuments,
+            'countRegions' => $this->countRegions,
+            'countPeriods' => $this->countPeriods,
+            'countTargets' => $this->countTargets,
+            'countUsers' => $this->countUsers,
+            'countCategories' => $this->countCategories,
+            
         ]);
     }
 }
