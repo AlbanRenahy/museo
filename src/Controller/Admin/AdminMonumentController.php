98824c8434fbedcc6799d0cd1db2ba2aed3eb1b6
amtbzh <amtbzh@gmail.com>
2020-03-11 14:32:08 +0100

    add: Table for Monument in Dashboard

--- src/Controller/Admin/AdminMonumentController.php
+++ src/Controller/Admin/AdminMonumentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\MonumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,8 +15,9 @@ class AdminMonumentController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(MonumentRepository $monumentRepository)
     {
-        return $this->render('dashboard/index.html.twig');
+        $monuments = $monumentRepository->findAll();
+        return $this->render('dashboard/monument/index.html.twig', compact('monuments'));
     }
 }
