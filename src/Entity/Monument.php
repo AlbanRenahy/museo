e0adccd637bfc12138fc0aaa1d14fc900b5b5c63
amtbzh <amtbzh@gmail.com>
2020-03-11 09:47:58 +0100

    miss: to add Component

--- src/Entity/Monument.php
+++ src/Entity/Monument.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ApiResource
