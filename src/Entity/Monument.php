5d666fcb3f9fd4ceb3d4dc7c669523cff066d95f
amtbzh <amtbzh@gmail.com>
2020-03-12 14:59:33 +0100

    try: To groups API Platform

--- src/Entity/Monument.php
+++ src/Entity/Monument.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
