7d34c997961d30a71a144cb569621d53764f5081
amtbzh <amtbzh@gmail.com>
2020-03-13 13:39:12 +0100

    start: CRUD for User

---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/user", name="dashboard.user.")
+ */
+class AdminUserController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+        return $this->render('dashboard/user/index.html.twig', compact('users'));
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(Request $req)
+    {
+        // $form = $this->createForm(RegionType::class, $region);
+        // $form->handleRequest($req);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $entityManager = $this->getDoctrine()->getManager();
+        //     $entityManager->persist($region);
+        //     $entityManager->flush();
+        //     $this->addFlash('success', 'Vous avez modifiez la région');
+
+        //     return $this->redirectToRoute('dashboard.region.index', ['id' => $region->getId()]);
+        // }
+
+
+        // return $this->render('dashboard/region/edit.html.twig', [
+        //     'form' => $form->createView(),
+        // ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(Request $req): Response
+    {
+        // $region = new Region();
+
+        // $form = $this->createForm(RegionType::class, $region);
+        // $form->handleRequest($req);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $entityManager = $this->getDoctrine()->getManager();
+        //     $entityManager->persist($region);
+        //     $entityManager->flush();
+        //     $this->addFlash('success', 'Région bien ajouté');
+
+        //     return $this->redirectToRoute('dashboard.region.index', ['id' => $region->getId()]);
+        // }
+
+        // return $this->render('dashboard/region/new.html.twig', [
+        //     'form' => $form->createView(),
+        // ]);
+
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete($id): Response
+    {
+        // $em = $this->getDoctrine()->getManager();
+        // $obj = $em->getRepository(Region::class)->find($id);
+        // $em->remove($obj);
+        // $em->flush();
+        // $this->addFlash('danger', 'Vous avez bien supprimer la région');
+        // return $this->redirectToRoute('dashboard.region.index');
+    }
+}
