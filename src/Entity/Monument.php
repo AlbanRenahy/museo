e133a6964b0670f603422f79fb60db09334405dc
amtbzh <amtbzh@gmail.com>
2020-03-18 10:39:21 +0100

    add: Filename field in DB

--- src/Entity/Monument.php
+++ src/Entity/Monument.php
@@ -151,6 +151,11 @@ class Monument
      */
     private $isActive;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $filename;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -330,6 +335,18 @@ class Monument
     public function setIsActive(?bool $isActive): self
     {
         $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
 
         return $this;
     }
