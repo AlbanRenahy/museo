d346ef89880a5cb388d09f556c0ec47c61853039
amtbzh <amtbzh@gmail.com>
2020-03-18 13:03:28 +0100

    del: Some inutile things

--- doctrine_migration_20200316182028.sql
+++
@@ -1,5 +0,0 @@
--- Doctrine Migration File Generated on 2020-03-16 18:20:28
-
--- Version 20200316182011
-ALTER TABLE monument ADD is_active TINYINT(1) DEFAULT NULL, CHANGE region_id region_id INT NOT NULL;
-INSERT INTO migration_versions (version, executed_at) VALUES ('20200316182011', CURRENT_TIMESTAMP);
